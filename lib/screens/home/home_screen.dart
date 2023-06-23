ce846ebc3864407dfe15a10c0a4f5f97fd3ab145
Tran Quoc Thinh <thinhtq.031100@gmail.com>
2023-06-23 11:46:50 +0700

    Added sound test data file

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:go_router/go_router.dart';
+import 'package:ipa_handbook/config/constants/assets.dart';
 
 import '../../config/router/screen_name.dart';
 import 'models/sound_preview.dart';
@@ -13,37 +16,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  late final List<SoundPreview> sounds;
-
-  @override
-  void initState() {
-    super.initState();
-    sounds = [
-      SoundPreview.fromJson('''{
-        "crawl_key": "b",
-        "name": "b",
-        "example": "buy | verb | about",
-        "type": "consonant"
-    }'''),
-      SoundPreview.fromJson('''{
-        "crawl_key": "b",
-        "name": "b",
-        "example": "buy | verb | about",
-        "type": "consonant"
-    }'''),
-      SoundPreview.fromJson('''{
-        "crawl_key": "b",
-        "name": "b",
-        "example": "buy | verb | about",
-        "type": "consonant"
-    }'''),
-      SoundPreview.fromJson('''{
-        "crawl_key": "b",
-        "name": "b",
-        "example": "buy | verb | about",
-        "type": "consonant"
-    }'''),
-    ];
+  Future<List> readJsonFile(String filePath) async {
+    var input = await rootBundle.loadString(Temp.soundTestData);
+    var list = jsonDecode(input);
+    return list;
   }
 
   @override
@@ -60,13 +36,31 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ],
       ),
-      body: GridView.builder(
-          itemCount: sounds.length,
-          gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-          itemBuilder: (context, index) {
-            return PreviewElement(
-              soundPreview: sounds[index],
-            );
+      body: FutureBuilder(
+          future: readJsonFile(Temp.soundTestData),
+          builder: (context, snapshot) {
+            if (snapshot.hasError) {
+              return const Center(
+                child: Text('Something went wrong'),
+              );
+            }
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+
+            final List list = snapshot.data as List;
+            final List<SoundPreview> sounds =
+                list.map((e) => SoundPreview.fromJson(jsonEncode(e))).toList();
+            return GridView.builder(
+                itemCount: sounds.length,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+                itemBuilder: (context, index) {
+                  return PreviewElement(
+                    soundPreview: sounds[index],
+                  );
+                });
           }),
     );
   }
